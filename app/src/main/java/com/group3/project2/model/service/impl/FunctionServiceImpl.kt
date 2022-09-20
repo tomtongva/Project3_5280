49d174312730ddd7d06b0e38cf9f489501c329df
almiller <almiller@veracode.com>
2022-09-20 10:45:24 -0400

    Initialized FunctionService

---
+++ app/src/main/java/com/group3/project2/model/service/impl/FunctionServiceImpl.kt
@@ -0,0 +1,91 @@
+package com.group3.project2.model.service.impl
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
+import com.group3.project2.model.service.FunctionService
+
+class FunctionServiceImpl : FunctionService{
+
+    private var functions: FirebaseFunctions
+
+    init {
+        functions = Firebase.functions
+    }
+
+    override fun createNewGame(text: String) : Task<String> {
+        val data = hashMapOf(
+            "text" to text,
+            "push" to true
+        )
+
+        return functions
+            .getHttpsCallable("createNewGame")
+            .call(data)
+            .continueWith { task ->
+                val result = task.result?.data as String
+                result
+            }
+    }
+
+    override fun joinGame(text: String) : Task<String>{
+        val data = hashMapOf(
+            "text" to text,
+            "push" to true
+        )
+
+        return functions
+            .getHttpsCallable("createNewGame")
+            .call(data)
+            .continueWith { task ->
+                val result = task.result?.data as String
+                result
+            }
+    }
+
+    override fun drawCard(text: String) : Task<String>{
+        val data = hashMapOf(
+            "text" to text,
+            "push" to true
+        )
+
+        return functions
+            .getHttpsCallable("createNewGame")
+            .call(data)
+            .continueWith { task ->
+                val result = task.result?.data as String
+                result
+            }
+    }
+
+    override fun playCard(text: String) : Task<String>{
+        val data = hashMapOf(
+            "text" to text,
+            "push" to true
+        )
+
+        return functions
+            .getHttpsCallable("createNewGame")
+            .call(data)
+            .continueWith { task ->
+                val result = task.result?.data as String
+                result
+            }
+    }
+
+    override fun leaveGame(text: String) : Task<String>{
+        val data = hashMapOf(
+            "text" to text,
+            "push" to true
+        )
+
+        return functions
+            .getHttpsCallable("createNewGame")
+            .call(data)
+            .continueWith { task ->
+                val result = task.result?.data as String
+                result
+            }
+    }
+}
